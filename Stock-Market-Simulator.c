6676c53b27b37d604ef0b39339969378a8973f03
Shreeyans2305 <shreeyans.vichare@gmail.com>
2025-04-12 00:30:52 +0530

    Stocks Added

--- Stock-Market-Simulator.c
+++ Stock-Market-Simulator.c
@@ -18,7 +18,7 @@ struct Stock
     float deviation;
     int user_holding;
 };
-
+void Initilize_Stocks(struct Stock stocks[20]);
 void main()
 {
     //User initialization 
@@ -50,16 +50,114 @@ void main()
 
     //Initializing Stocks
     struct Stock stocks[20];
-    stocks[0].name = "APPLE";
-    stocks[0].symbol = "APL";
-    stocks[0].current_price = 255.25;
-    stocks[0].user_holding = 0;
+    Initilize_Stocks(stocks);
+    //Displaying Stocks
+    /*
     printf("\n Name \t Symbol \t Stock Price \t Deviation");
-    printf("\n %s \t %s \t\t %.2f \t (+2.39%)",stocks[0].name,stocks[0].symbol,stocks[0].current_price);
+    printf("\n %s \t %s \t\t %.2f \t (+2.39)",stocks[0].name,stocks[0].symbol,stocks[0].current_price);*/
+}
+
+void Initilize_Stocks(struct Stock stocks[20])
+{
+    stocks[0].name = "APPLE";
+    stocks[0].symbol = "AAPL";
+    stocks[0].current_price = 1707.20;
+    stocks[0].user_holding = 0;
 
     stocks[1].name = "MICROSOFT";
     stocks[1].symbol = "MSFT";
-    stocks[1].current_price = 123.48;
-    stocks[1].user_holding = 1;
-    //Displaying Stocks
+    stocks[1].current_price = 3300.00;
+    stocks[1].user_holding = 0;
+
+    stocks[2].name = "NVIDIA";
+    stocks[2].symbol = "NVDA";
+    stocks[2].current_price = 949.40;
+    stocks[2].user_holding = 0;
+
+    stocks[3].name = "AMAZON";
+    stocks[3].symbol = "AMZN";
+    stocks[3].current_price = 1576.30;
+    stocks[3].user_holding = 0;
+
+    stocks[4].name = "BERKSHIRE HATHWAY";
+    stocks[4].symbol = "BRK-B";
+    stocks[4].current_price = 4496.50;
+    stocks[4].user_holding = 0;
+
+    stocks[5].name = "ALPHABET (GOOGLE)";
+    stocks[5].symbol = "GOOG";
+    stocks[5].current_price = 1369.50;
+    stocks[5].user_holding = 0;
+
+    stocks[6].name = "META";
+    stocks[6].symbol = "META";
+    stocks[6].current_price = 4643.60;
+    stocks[6].user_holding = 0;
+
+    stocks[7].name = "TESLA";
+    stocks[7].symbol = "TSLA";
+    stocks[7].current_price = 2139.80;
+    stocks[7].user_holding = 0;
+
+    stocks[8].name = "TOYOTA";
+    stocks[8].symbol = "TM";
+    stocks[8].current_price = 1452.70;
+    stocks[8].user_holding = 0;
+
+    stocks[9].name = "VISA";
+    stocks[9].symbol = "V";
+    stocks[9].current_price = 2881.90;
+    stocks[9].user_holding = 0;
+
+    stocks[10].name = "TATA STEEL";
+    stocks[10].symbol = "TTSL";
+    stocks[10].current_price = 133.42;
+    stocks[10].user_holding = 0;
+
+    stocks[11].name = "CANARA BANK";
+    stocks[11].symbol = "CANBK";
+    stocks[11].current_price = 90.53;
+    stocks[11].user_holding = 0;
+
+    stocks[12].name = "INFOSYS";
+    stocks[12].symbol = "INFY";
+    stocks[12].current_price = 1416.80;
+    stocks[12].user_holding = 0;
+
+    stocks[13].name = "RELIANCE INDUSTRIES";
+    stocks[13].symbol = "REL";
+    stocks[13].current_price = 1220.00;
+    stocks[13].user_holding = 0;
+
+    stocks[14].name = "TATA MOTORS";
+    stocks[14].symbol = "TTMR";
+    stocks[14].current_price = 596.00;
+    stocks[14].user_holding = 0;
+
+    stocks[15].name = "HDFC BANK";
+    stocks[15].symbol = "HDFC";
+    stocks[15].current_price = 1805.20;
+    stocks[15].user_holding = 0;
+
+    stocks[16].name = "BHARAT ELECTRONICS";
+    stocks[16].symbol = "BEL";
+    stocks[16].current_price = 285.30;
+    stocks[16].user_holding = 0;
+
+    stocks[17].name = "STATE BANK OF INDIA";
+    stocks[17].symbol = "SBI";
+    stocks[17].current_price = 753.85;
+    stocks[17].user_holding = 0;
+
+    stocks[18].name = "PUNJAB NATIONAL BANK";
+    stocks[18].symbol = "PNB";
+    stocks[18].current_price = 96.08;
+    stocks[18].user_holding = 0;
+
+    stocks[19].name = "ICICI BANK";
+    stocks[19].symbol = "ICICI";
+    stocks[19].current_price = 1312.05;
+    stocks[19].user_holding = 0;
+
+    printf("\nStocks Initialized Sucessfully");
 }
